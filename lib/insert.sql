5c882580e45f28b8f49f0dccecaa24d976595b21
Cyrus Mureithi <cmraey@gmail.com>
2024-08-01 13:50:17 +0300

    Easy Eeasy

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm'),
+('Rowdy', 8, 'M', 'Black', 'Aggressive'),
+('Tabitha', 6, 'F', 'Golden', 'Friendly'),
+('Sergeant Brown', 12, 'M', 'Brown', 'Aggressive'),
+('Melissa', 7, 'F', 'Brown', 'Friendly'),
+('Grinch', 15, 'M', 'Green', 'Grumpy'),
+('Wendy', 5, 'F', 'White', 'Playful'),
+(NULL, 3, 'F', 'Black', 'Shy');
